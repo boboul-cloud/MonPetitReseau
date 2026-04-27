55fadb94236ffa8b1a275357e4e7792523ab7842
BudgetApp <bob.oulhen@gmail.com>
2026-04-27 15:18:46 +0200

    Initial Commit

---
+++ MonPetitReseau/MonPetitReseauApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonPetitReseauApp.swift
+//  MonPetitReseau
+//
+//  Created by Robert Oulhen on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MonPetitReseauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
